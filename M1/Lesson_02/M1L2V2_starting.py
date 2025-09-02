2335915a5e8affeda821ec1cfb3729aedb684791
chhk <chorhatarahuduketuri@gmail.com>
2025-09-02 12:47:48 +0100

    Added streamlit cache, spinner, and slider

--- M1/Lesson_02/M1L2V2_starting.py
+++ M1/Lesson_02/M1L2V2_starting.py
@@ -2,6 +2,17 @@ from dotenv import load_dotenv
 import openai
 import streamlit as st
 
+@st.cache_data
+def get_response(user_prompt: str, temperature: float):
+    response = client.responses.create(
+        model="gpt-4o",
+        input=[{"role": "user", "content": user_prompt}
+               ],
+        temperature=temperature,
+        max_output_tokens=100
+    )
+    return response
+
 load_dotenv()
 
 client = openai.OpenAI()
@@ -9,12 +20,17 @@ client = openai.OpenAI()
 st.title("Hello, GenAI!")
 st.write("This is my first Streamlit app.! (well it's not I was among the first adopters but it's been years so let's just pretend, ok?)")
 
-response = client.responses.create(
-    model="gpt-4o",
-    input=[{"role": "user", "content": "Explain generative AI in one sentence."}
-           ],
-    temperature=0.7,
-    max_output_tokens=100
+user_prompt = st.text_input("Enter your prompt:", "Explain generative AI in one sentence.")
+
+temperature = st.slider(
+    "Model temperature:",
+    min_value=0.0,
+    max_value=1.0,
+    value=0.7,
+    step=0.01,
+    help="Controls randomness: 0 = deterministic, 1 = very creative"
 )
 
-st.write(response.output[0].content[0].text)
+with st.spinner("AI is working..."):
+    response = get_response(user_prompt, temperature)
+    st.write(response.output[0].content[0].text)
